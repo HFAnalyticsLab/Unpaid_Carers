4762dfb9df82dccb771950aa09e95fc36fcc176d
Anne Alarilla <anne.alarilla@health.org.uk>
2021-04-16 16:05:12 +0100

    Update variables selected

--- src/0_data_loading.R
+++ src/0_data_loading.R
@@ -14,16 +14,17 @@ library(stringr)
 library(here)
 library(janitor)
 library(readxl)
+library(data.table)
+
 
 ##Reading in the variables I want
 variables <- read_excel(here::here("Variables.xlsx"))
 variables <- variables %>% 
   remove_empty( c("rows", "cols")) %>% 
   mutate(Name=gsub("XXX","",Name)) %>% 
-  mutate(Name=gsub("cf_","",Name))
+  mutate(Name=gsub("cf_","",Name)) 
 
 t<-variables$Name
-
 
 ##Setting the working directory for data
 setwd(here::here('data'))
@@ -34,8 +35,8 @@ listind <- list.files(here::here('data'), pattern = "*_indresp")
 #Loading the data set
 ind <- lapply(listind, read_sav)
 
-##Extracting the data set with only the caring variables that we're interested in
-ind <- lapply(ind, function(x) x%>% select(contains(t)))
+##Extracting the data set with only the variables that we're interested in
+ind<-lapply(ind, function(x) x%>% select(contains(t),-ends_with(c("st","finishtime","starttime","duration","end"))))
 
 ##Naming the data based on the waves
 names(ind) <- gsub("\\.sav$", "", listind)
@@ -44,21 +45,14 @@ names(ind) <- gsub("\\.sav$", "", listind)
 list2env(ind, .GlobalEnv)
 
 ##saving selected variables
+
 saveRDS(j_indresp, here::here('data', 'care_type', 'wave10.rds'))
 
 #Combining the telephone and web waves
+
 post<-cf_indresp_w %>% 
   bind_rows(cf_indresp_t) %>% 
-  full_join(cg_indresp_w, by= "pidp") %>% 
-  ##NAs= not taken part in both surveys, so taking the most recent data available
-  #-10 means didn't take part in wave 6 but took part in wave 7
-  mutate(caring=ifelse(is.na(cf_caring),-10,as.numeric(cf_caring)), 
-         aidhh=ifelse(is.na(cg_aidhh), cf_aidhh, cg_aidhh), 
-         aidhrs=ifelse(is.na(cg_aidhrs_cv),cf_aidhrs_cv,cg_aidhrs_cv),
-         psu=ifelse(is.na(psu.y),psu.x,psu.y),
-         strata=ifelse(is.na(strata.y),strata.x,strata.y),
-         weight=ifelse(is.na(cg_betaindin_xw),cf_betaindin_xw_t,cg_betaindin_xw))
- 
+  full_join(cg_indresp_w, by= "pidp")
 
  
 saveRDS(post, here::here('data', 'care_type', 'wave6n7_covid.rds'))
