c6b60071483df4b4eeec79e5aaa2682314a875c6
Anne Alarilla <anne.alarilla@health.org.uk>
2021-04-19 13:38:24 +0100

    Load selected variables

--- src/0_data_loading.R
+++ src/0_data_loading.R
@@ -1,12 +1,13 @@
 
-# =======================================================
+##
 # Project: Unpaid Carers with high care level during the pandemic
 # Purpose: Derive the care type variables 
 # Author: Anne Alarilla
 # Date: 11/04/2021
-# =======================================================
+##
 
-#Load libraries
+# Load library ------------------------------------------------------------
+
 library(haven)
 library(tidyr)
 library(tidyverse)
@@ -14,10 +15,9 @@ library(stringr)
 library(here)
 library(janitor)
 library(readxl)
-library(data.table)
 
 
-##Reading in the variables I want
+# Selected Variable list --------------------------------------------------
 variables <- read_excel(here::here("Variables.xlsx"))
 variables <- variables %>% 
   remove_empty( c("rows", "cols")) %>% 
@@ -26,17 +26,19 @@ variables <- variables %>%
 
 t<-variables$Name
 
-##Setting the working directory for data
-setwd(here::here('data'))
+
+# Download Understanding society data set ---------
 
 #Create lists of the _indresp files-The main data set
 listind <- list.files(here::here('data'), pattern = "*_indresp")
 
-#Loading the data set
+#Loading the data sets
 ind <- lapply(listind, read_sav)
 
-##Extracting the data set with only the variables that we're interested in
-ind<-lapply(ind, function(x) x%>% select(contains(t),-ends_with(c("st","finishtime","starttime","duration","end"))))
+##Extracting the data sets with only the variables that we're interested in
+ind<-lapply(ind, function(x) x%>% select(contains(t),-ends_with(c("st","finishtime",
+                                                                  "starttime","duration","end",
+                                                                  "outcome", "hidp","useragentstring"))))
 
 ##Naming the data based on the waves
 names(ind) <- gsub("\\.sav$", "", listind)
@@ -44,16 +46,45 @@ names(ind) <- gsub("\\.sav$", "", listind)
 ##saving each data set as separate files in the data environment
 list2env(ind, .GlobalEnv)
 
-##saving selected variables
+
+# Saving data -------------------------------------------
+
+# Pre Pandemic ------------------------------------------------------------
+
 
 saveRDS(j_indresp, here::here('data', 'care_type', 'wave10.rds'))
 
+
+# COVID-19 Data -----------------------------------------------------------
+
+
 #Combining the telephone and web waves
+#Removing data labels
+val_labels(cg_indresp_w) <- NULL
+var_label(cg_indresp_w) <- NULL
 
-post<-cf_indresp_w %>% 
+#Turning into long format
+cg_long<-cg_indresp_w %>% 
+  select_if(~!is.character(.)) %>%  
+  pivot_longer(!c(pidp,psu, strata,birthy, racel_dv), names_to="var", values_to="cg") %>% 
+  separate(var, into=c("wave","var"), sep="_", extra="merge") 
+
+cf_long<-cf_indresp_w %>% 
   bind_rows(cf_indresp_t) %>% 
-  full_join(cg_indresp_w, by= "pidp")
+  mutate_if(is.numeric, ~replace(., is.na(.), -6)) %>% 
+  select_if(~!is.character(.)) %>% 
+  pivot_longer(!c(pidp,psu, strata,birthy, racel_dv), names_to="var", values_to="cf") %>% 
+  separate(var, into=c("wave","var"), sep="_", extra="merge") 
 
- 
-saveRDS(post, here::here('data', 'care_type', 'wave6n7_covid.rds'))
+#Joining wave 6 and 7, taking data from wave 7 if available, if not then using wave 6 
+during<-cg_long %>%
+  select(-wave) %>% 
+  full_join(cf_long %>% 
+            select(-wave), by=c("pidp", "psu", "strata", "birthy", "racel_dv", "var")) %>% 
+  mutate(value=ifelse(is.na(cg),cf,cg)) %>% 
+  select(-c(cf, cg)) %>% 
+  pivot_wider(names_from ="var", values_from ="value") %>% 
+  #Replacing the NAs to -6, NA because did not take part in both surveys (just one) or did not take part in telephone only variables
+  mutate_if(is.numeric, ~replace(., is.na(.), -6))
 
+saveRDS(during, here::here('data', 'care_type', 'wave6n7_covid.rds'))
