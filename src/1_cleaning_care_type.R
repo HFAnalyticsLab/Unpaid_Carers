54eaa21d02b371af9d8d658930d3b6744921260c
Anne Alarilla <anne.alarilla@health.org.uk>
2021-05-28 14:35:27 +0100

    Remove unecessary coded variables

--- src/1_cleaning_care_type.R
+++ src/1_cleaning_care_type.R
@@ -39,7 +39,7 @@ all<-all %>%
                                aidhh==1 & aidhrs_cv %notin% as.character(c(4:7,9))~ 1,
                                caring==1 & carehow3==0~ 1,
                                carer==2 ~ 3),
-    carer_pre=case_when((j_aidxhh==1|j_aidhh==1)~1,
+         carer_pre=case_when((j_aidxhh==1|j_aidhh==1)~1,
                              j_aidxhh==2 & j_aidhh==2 ~2,
                              j_aidxhh==2 & j_aidhh<0 ~2,
                              j_aidxhh<0 & j_aidhh==2~2),
@@ -48,24 +48,12 @@ all<-all %>%
                                    carer_pre==2 ~ 3),
          care_loc_pre=case_when(j_aidhh==1 &(j_aidxhh==2|j_aidxhh<0)~1,
                                 j_aidxhh==1& (j_aidhh==2|j_aidhh<0)~2,
-                                j_aidhh==1& j_aidxhh==1~3),
+                                j_aidhh==1& j_aidxhh==1~3,
+                                j_aidhh==2& j_aidxhh==2~4),
          care_loc_cv=case_when(aidhh==1& (caring==2|caring<0)~1,
                                caring==1& (aidhh==2|aidhh<0)~2,
-                               aidhh==1&caring==1~3),
-    ##Check this
-         care_status=case_when(carer_pre==1 & carer==1~1,
-                               carer_pre==2& carer==1~2,
-                               carer_pre==1&carer==2~3),
-         care_loc_change=case_when(care_loc_pre==1&care_loc_cv==1~1, 
-                                   care_loc_pre==2&care_loc_cv==2~1,
-                                   care_loc_pre==3&care_loc_cv==3~1,
-                                   care_loc_pre==1&(care_loc_cv==2|care_loc_cv==3)~2,
-                                   care_loc_pre==2&(care_loc_cv==1|care_loc_cv==3)~2,
-                                   care_loc_pre==3&(care_loc_cv==1|care_loc_cv==2)~2),
-         care_type_change=case_when(care_hours_pre==1&care_hours==1~1,
-                                    care_hours_pre==2&care_hours==2~1,
-                                    care_hours_pre==1&care_hours==2~2,
-                                    care_hours_pre==2&care_hours==1~2))
+                               aidhh==1&caring==1~3,
+                               aidhh==2&caring==2~4))
 
 
 ##replacing all the NAs
@@ -80,27 +68,20 @@ all_lab$carer<-factor(all_lab$carer, levels=c(1,2),
 
 all_lab$care_hours<-factor(all_lab$care_hours, levels=c(3,1,2), labels=c("No caring","Low Level Caring", "High Level Caring"))
 
-all_lab$care_loc_cv<- factor(all_lab$care_loc_cv, levels=c(1,2,3), 
-                             labels=c("Within household only", "Outside of household only","Within and outside household"))
+all_lab$care_loc_cv<- factor(all_lab$care_loc_cv, levels=c(1,2,3,4), 
+                             labels=c("Within household only", "Outside of household only","Within and outside household", "No caring"))
 
 all_lab$carer_pre<-factor(all_lab$carer_pre, levels=c(1,2), 
                            labels=c("Yes", "No"))
 all_lab$care_hours_pre<-factor(all_lab$care_hours_pre, levels=c(3,1,2), 
                                labels=c("No caring","Low Level Caring", "High Level Caring"))
 
-all_lab$care_loc_pre<-factor(all_lab$care_loc_pre, levels=c(1,2,3), 
-                              labels=c("Within household only", "Outside of household only","Within and outside household"))
-
-all_lab$care_loc_change<- factor(all_lab$care_loc_change, levels=c(1,2), 
-                                 labels=c("No", "Yes"))
-all_lab$care_type_change<-factor(all_lab$care_type_change, levels=c(1,2), 
-                                  labels=c("No", "Yes"))
-all_lab$care_status<-factor(all_lab$care_status, levels=c(1,2,3), 
-                             labels=c("Caring pre and during", "New carers during pandemic", "Stopped caring during pandemic"))
+all_lab$care_loc_pre<-factor(all_lab$care_loc_pre, levels=c(1,2,3,4), 
+                              labels=c("Within household only", "Outside of household only","Within and outside household", "No caring"))
 
 
-summary(all_lab[,c("carer","care_hours","care_loc_cv","carer_pre",
-                   "care_loc_change", "care_type_change","care_hours_pre","care_loc_pre","care_status")])
+
+summary(all_lab[,c("carer","care_hours","care_loc_cv","carer_pre","care_hours_pre","care_loc_pre")])
 
 
 saveRDS(all_lab, here::here('data', 'care_type', 'caring_pandemic_care_type.rds'))
